6beff2080df9ffb6509ee0f3e093adcfa814b6dd
Matteo Elias Schapira <54103711+Niccolo10@users.noreply.github.com>
2026-01-14 17:10:39 +0100

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,4 @@
-alert('script source works')
+// Change the heading of the page
+document.querySelector('h1').innerText = "Whitek goldengoose PoC";
+// Change a "Submit" button to red
+document.querySelector('button').style.backgroundColor = "red";
